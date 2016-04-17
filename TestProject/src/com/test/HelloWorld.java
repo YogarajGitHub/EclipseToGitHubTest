2e39693af698347e288fbea410e1e64a32f04546
Yoga <Yoga@192.168.0.102>
2016-04-17 15:38:10 -0400

    Initial commit

---
+++ TestProject/src/com/test/HelloWorld.java
@@ -0,0 +1,15 @@
+package com.test;
+
+import org.testng.annotations.Test;
+
+public class HelloWorld {
+public static void main(String arg[]){
+	System.out.println("Hello Mac");
+}
+
+@Test
+public void tst(){
+	
+}
+}
+
